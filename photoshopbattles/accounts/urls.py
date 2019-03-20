11545ddaa413e8f1ed5ca0f2e17abdd156a912c9
Austin Mallar <austin.mallar@student.ufv.ca>
2019-03-20 12:54:12 -0700

    Initial setup of Django app. Added basic auth

---
+++ photoshopbattles/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import login_view,register_view,logout_view
+
+urlpatterns = [
+    path('login/', login_view),
+    path('register/', register_view),
+    path('logout/', logout_view)
+]
