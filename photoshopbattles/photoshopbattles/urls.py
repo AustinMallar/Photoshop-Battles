92ce1d054778501566867cbfc7215d9177e058be
Austin Mallar <austin.mallar@student.ufv.ca>
2019-04-01 22:01:29 -0700

    Changed accounts path to just account.

--- photoshopbattles/photoshopbattles/urls.py
+++ photoshopbattles/photoshopbattles/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'', include(tf_urls)),
     path('admin/', admin.site.urls),
     path('',include('application.urls',namespace='application')),
-    path('accounts/',include('accounts.urls'))
+    path('account/',include('accounts.urls'))
 ]
 
 if settings.DEBUG:
