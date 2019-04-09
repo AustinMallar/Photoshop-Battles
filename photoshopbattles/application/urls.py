4cc0a8cb4ea2cafd74b089427f9d4941a364ad67
Austin Mallar <austin.mallar@student.ufv.ca>
2019-04-09 12:32:05 -0700

    Only logged in users can post reply.

--- photoshopbattles/application/urls.py
+++ photoshopbattles/application/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path,include
-from .views import leaderboard_view, profile_view, new_post
+from .views import IndexView,leaderboard_view, profile_view, new_post
 
 app_name = 'application'
 
 urlpatterns = [
-    path('', views.IndexView, name='index'),
+    path('', IndexView, name='index'),
     path('leaderboard', leaderboard_view),
     path('profile', profile_view),
     path('new_post', new_post),
