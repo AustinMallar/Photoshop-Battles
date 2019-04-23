6e3fb6c01724dfa2d5182aec5a49bfc41e388e14
Austin Mallar <austin.mallar@student.ufv.ca>
2019-04-22 22:45:26 -0700

    Leaderboard top users working

--- photoshopbattles/accounts/models.py
+++ photoshopbattles/accounts/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from application.models import Reply
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
     bio = models.TextField(max_length=400, default='Bio..')
+
+    def total_number_likes(self):
+        likes = 0
+        replies = Reply.objects.filter(user=self.user)
+        for r in replies:
+            likes+=len(r.like_list())
+        return int(likes)
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
--- photoshopbattles/application/urls.py
+++ photoshopbattles/application/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path,include
-from .views import IndexView,leaderboard_view, profile_view, new_post, delete_post, delete_reply, like_reply, unlike_reply, favourite_reply, unfavourite_reply
+from .views import IndexView,leaderboard_view, new_post, delete_post, delete_reply, like_reply, unlike_reply, favourite_reply, unfavourite_reply
 
 app_name = 'application'
 
 urlpatterns = [
     path('', IndexView, name='index'),
     path('leaderboard', leaderboard_view),
-    path('profile', profile_view),
     path('new_post', new_post),
     path('delete_post/<int:id>',delete_post),
     path('delete_reply/<int:id>',delete_reply),
