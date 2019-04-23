54bfabc076ca437ba930ee1504bb002053e2c6c6
Priya Kalra <priya.kalra@student.ufv.ca>
2019-04-23 00:19:59 -0700

    fixed some templates

--- photoshopbattles/accounts/forms.py
+++ photoshopbattles/accounts/forms.py
@@ -9,7 +9,7 @@ from django.contrib.auth import (
 
 User = get_user_model()
 
-
+# EditProfileForm allows the user to edit their bio and image
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
@@ -24,7 +24,8 @@ class EditProfileForm(forms.ModelForm):
         
         return super(EditProfileForm, self).clean(*args, **kwargs)
 
-
+# UserRegisterForm allows the user to register with
+# a username, email address and password
 class UserRegisterForm(forms.ModelForm):
     email = forms.EmailField(label='Email address')
     email2 = forms.EmailField(label='Confirm Email')
@@ -46,6 +47,5 @@ class UserRegisterForm(forms.ModelForm):
             raise forms.ValidationError("Emails must match")
         email_qs = User.objects.filter(email=email)
         if email_qs.exists():
-            raise forms.ValidationError(
-                "This email has already been registered")
+            raise forms.ValidationError("This email has already been registered")
         return super(UserRegisterForm, self).clean(*args, **kwargs)
--- photoshopbattles/accounts/urls.py
+++ photoshopbattles/accounts/urls.py
@@ -5,7 +5,7 @@ from .views import register_view,logout_view,profile_view,profile_edit_view
 urlpatterns = [
     path('account/register/', register_view),
     path('account/logout/', logout_view),
-    path('profile/<str:username>',profile_view),
-    path('account/edit-profile',profile_edit_view)
+    path('profile/<str:username>', profile_view),
+    path('account/edit-profile', profile_edit_view)
 ]
 
